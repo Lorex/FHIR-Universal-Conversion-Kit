f1afecb33a409f0ff14f82bafe63804bff3495f1
郵差 <twHenryYang@gmail.com>
2022-09-07 17:10:25 +0800

    feat: convert Organization.identifier

--- profile/organizationMS.js
+++ profile/organizationMS.js
@@ -47,4 +47,112 @@ module.exports.fields = [
             }
         }
     },
+    {
+        source: 'identifier',
+        target: 'Organization.identifier',
+        beforeConvert: (data) => {
+            return {
+                // 忽略identifier紀載的內容，使用Identifier子項目來組合
+            }
+        }
+    },
+    {
+        source: 'identifier.use',
+        target: 'Organization.identifier',
+        beforeConvert: (data) => {
+            return {
+                use: data, //HL7 FHIR 提供以下選項usual | official | temp | secondary | old (If known)
+            }
+        }
+    },
+    {
+        source: 'identifier.type',
+        target: 'Organization.identifier',
+        beforeConvert: (data) => {
+            return {
+                // 忽略identifier.type紀載的內容，使用CodeableConcept子項目來組合
+            }
+        }
+    },
+    {
+        source: 'identifier.type.coding',
+        target: 'Organization.identifier',
+        beforeConvert: (data) => {
+            return {
+                type: {
+                    coding: {
+                        // 忽略identifier.type.coding紀載的內容，使用Coding子項目來組合
+                    }
+                }
+            }
+        }
+    },
+    {
+        source: 'identifier.type.coding.system',
+        target: 'Organization.identifier',
+        beforeConvert: (data) => {
+            return {
+                type: {
+                    coding: {
+                        system: data
+                    }
+                }
+            }
+        }
+    },
+    {
+        source: 'identifier.type.coding.code',
+        target: 'Organization.identifier',
+        beforeConvert: (data) => {
+            return {
+                type: {
+                    coding: {
+                        code: data
+                    }
+                }
+            }
+        }
+    },
+    {
+        source: 'identifier.type.coding.display',
+        target: 'Organization.identifier',
+        beforeConvert: (data) => {
+            return {
+                type: {
+                    coding: {
+                        display: data
+                    }
+                }
+            }
+        }
+    },
+    {
+        source: 'identifier.type.text',
+        target: 'Organization.identifier',
+        beforeConvert: (data) => {
+            return {
+                type: {
+                    text: data
+                }
+            }
+        }
+    },
+    {
+        source: 'identifier.system',
+        target: 'Organization.identifier',
+        beforeConvert: (data) => {
+            return {
+                system: data
+            }
+        }
+    },
+    {
+        source: 'identifier.value',
+        target: 'Organization.identifier',
+        beforeConvert: (data) => {
+            return {
+                value: data
+            }
+        }
+    },
 ]
