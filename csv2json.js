9127faab58b2be19587663e77bd683e03a2b148b
郵差 <twHenryYang@gmail.com>
2022-09-16 22:29:20 +0800

    update: prepare to create a customize covert func

--- csv2json.js
+++ csv2json.js
@@ -20,3 +20,29 @@ function csv2json(csvText) {
 
     return JSON.stringify(resultJSON);
 }
+
+function csv2json_organizationMS(csvText) {
+    // 根據不同FHIR profile來自定義csv轉換JSON的轉換邏輯
+    var lines = csvText.split("\n");
+
+    var resultJSON = [];
+
+    var headers = lines[0].split(",");
+
+    for (let i = 1; i < lines.length; i++) {
+        var obj = {};
+        var currentLine = lines[i].split(",");
+
+        for (let j = 0; j < currentLine.length; j++) {
+            if (currentLine[j] == "");// ignore blank value
+            else if(headers[j] == "identifier"){
+                // 根據identifier自定義轉換規則
+
+            }
+            else obj[headers[j]] = currentLine[j];
+        }
+        resultJSON.push(obj);
+    }
+
+    return JSON.stringify(resultJSON);
+}
