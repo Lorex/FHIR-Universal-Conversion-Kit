2ca4934bb2e4e046bc49474ee485681e4a1a4922
郵差 <twHenryYang@gmail.com>
2022-09-17 15:33:22 +0800

    feat: finish convert Organization.identifier

--- csv2json.js
+++ csv2json.js
@@ -41,14 +41,43 @@ function csv2json_organizationMS(csvText) {
                 if (currentLine[j + 2] != "")// ignore blank value
                     textObj["div"] = currentLine[j + 2];
 
-                obj[headers[j]] = textObj;
+                obj[headers[j]] = textObj;// export "text" object
+
                 j += 2;// pass the values that already converted
             }
             else if (headers[j] == "identifier") {// 組合identifier Object
+                let identifierObj = {};
+                if (currentLine[j + 1] != "")
+                    identifierObj["use"] = currentLine[j + 1];
 
+                let typeObj = {};
+                let codingObj = {};
+                if (currentLine[j + 4] != "")
+                    codingObj["system"] = currentLine[j + 4];
+                if (currentLine[j + 5] != "")
+                    codingObj["code"] = currentLine[j + 5];
+                if (currentLine[j + 6] != "")
+                    codingObj["display"] = currentLine[j + 6];
+
+                typeObj["coding"] = codingObj;
+                if (currentLine[j + 7] != "")
+                    typeObj["text"] = currentLine[j + 7];
+
+                identifierObj["type"] = typeObj;
+                if (currentLine[j + 8] != "")
+                    identifierObj["system"] = currentLine[j + 8];
+                if (currentLine[j + 9] != "")
+                    identifierObj["value"] = currentLine[j + 9];
+
+                obj[headers[j]] = identifierObj;// export "identifiter" object
+
+                j += 9;// pass the values that already converted
             }
+
             else if (currentLine[j] == "");// ignore blank value
+
             else obj[headers[j]] = currentLine[j];
+            // handle the rest of the top level keys for OrganizationMS Profile
         }
         resultJSON.push(obj);
     }
