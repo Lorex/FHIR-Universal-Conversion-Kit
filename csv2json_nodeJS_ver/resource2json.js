6493c8004da035a51a4862133ea39cc546c4e013
郵差 <twHenryYang@gmail.com>
2022-09-20 14:36:08 +0800

    refactor: rename variable in csv2json.js

--- csv2json_nodeJS_ver/resource2json.js
+++ csv2json_nodeJS_ver/resource2json.js
@@ -2,11 +2,11 @@ const csv2json = require('csvtojson');
 const fs = require('fs');
 
 csv2json().fromFile('./csvFiles/test.csv')
-    .then(users => {
-        console.log(users);
+    .then(results => {
+        console.log(results);
         fs.writeFile(
             './output/output.json',
-            JSON.stringify(users, null, 2),
+            JSON.stringify(results, null, 2),
             (err) => { if (err) throw err; }
         )
     });
