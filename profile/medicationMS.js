b03244e98d715d27e1be5a319e0e240381f665e3
郵差 <twHenryYang@gmail.com>
2022-10-08 21:52:12 +0800

    feat: Create Medication F.U.C.K convert profile

---
+++ profile/medicationMS.js
@@ -0,0 +1,39 @@
+module.exports.profile = {
+    name: 'medicationMS',
+    version: '1.0.0',
+    fhirServerBaseUrl: 'https://hapi.fhir.tw/fhir',
+    action: 'return', // return, upload
+    // fhirServerBaseUrl: 'http://140.131.93.149:8080/fhir',
+    // action: 'upload',
+}
+
+module.exports.globalResource = {
+    // Should be resource name
+    Medication: {
+        active: true,
+    }
+}
+
+module.exports.fields = [
+    {
+        source: 'id',
+        target: 'Medication.id',
+        beforeConvert: (data) => {
+            return `med-${data}`
+        }
+    },
+    {
+        source: 'text',
+        target: 'Medication.text',
+        // text已經組合成Object，直接回傳即可，無須再透過beforeConvert:()來處理
+    },
+    {
+        source: 'code',
+        target: 'Medication.code',
+        beforeConvert()
+    },
+    {
+        source: 'form',
+        target: 'Medication.form',
+    },
+]
