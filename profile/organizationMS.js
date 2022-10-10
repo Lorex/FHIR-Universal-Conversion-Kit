31e909b5d76735978196c835aac941afa2d2fb8b
郵差 <twHenryYang@gmail.com>
2022-10-10 22:32:51 +0800

    update: Still using return for FHIR format testing

--- profile/organizationMS.js
+++ profile/organizationMS.js
@@ -1,10 +1,10 @@
 module.exports.profile = {
     name: 'organizationMS',
     version: '1.0.0',
-    // fhirServerBaseUrl: 'https://hapi.fhir.tw/fhir',
-    // action: 'return', // return, upload
-    fhirServerBaseUrl: 'http://140.131.93.149:8080/fhir',
-    action: 'upload',
+    fhirServerBaseUrl: 'https://hapi.fhir.tw/fhir',
+    action: 'return', // return, upload
+    // fhirServerBaseUrl: 'http://140.131.93.149:8080/fhir',
+    // action: 'upload',
 }
 
 module.exports.globalResource = {
