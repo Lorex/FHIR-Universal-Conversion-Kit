58481a2e7446560d59e4d914d06f0f43eb794b83
郵差 <twHenryYang@gmail.com>
2022-11-05 15:48:02 +0800

    feat: Create F.U.C.K medication-TWCore profile base on medicationMS

---
+++ profile/medication-TWCore.js
@@ -0,0 +1,56 @@
+module.exports.profile = {
+  name: 'medication-TWCore',
+  version: '1.0.0',
+  // fhirServerBaseUrl: 'https://hapi.fhir.tw/fhir',
+  // action: 'return', // return, upload
+
+  // fhirServerBaseUrl: 'http://140.131.93.149:8080/fhir',
+  fhirServerBaseUrl: 'https://mitwfhir.dicom.org.tw/fhir',
+  action: 'upload',
+}
+
+module.exports.globalResource = {
+  // Should be resource name
+  Medication: {
+    meta: {
+      profile: [
+        "https://mitwfhir.dicom.org.tw/fhir/StructureDefinition/Medication-twcore"
+      ]
+    }
+  }
+}
+
+module.exports.fields = [
+  {
+    source: 'id',
+    target: 'Medication.id',
+    beforeConvert: (data) => {
+      return `twcore-verified-med-${data}`
+    }
+  },
+  {
+    source: 'text',
+    target: 'Medication.text',
+    // text已經組合成Object，直接回傳即可，無須再透過beforeConvert:()來處理
+  },
+  {
+    source: 'code',
+    target: 'Medication.code',
+    beforeConvert: (data) => {
+      let code = data;
+      code.coding = [code.coding];// 把coding按照FHIR Definition包成Array
+
+      return code;
+    }
+  },
+  {
+    source: 'form',
+    target: 'Medication.form',
+    beforeConvert: (data) => {
+      let form = data;
+      form.coding = [form.coding];
+
+      return form;
+    }
+  },
+]
