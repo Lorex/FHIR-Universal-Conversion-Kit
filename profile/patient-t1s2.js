5cca81e8d86936d3542f84109a40678c7c7be326
郵差 <twHenryYang@gmail.com>
2022-11-06 22:15:13 +0800

    feat: Create 3 Patient FUCK profiles for 3 different scenario validate meta.profile

---
+++ profile/patient-t1s2.js
@@ -0,0 +1,130 @@
+module.exports.profile = {
+  name: 'patient-t1s2',// track1 scenario2
+  version: '1.0.0',
+  // fhirServerBaseUrl: 'https://hapi.fhir.tw/fhir',
+  // action: 'return', // return, upload
+
+  // fhirServerBaseUrl: 'http://140.131.93.149:8080/fhir',
+  fhirServerBaseUrl: 'http://192.168.50.6:8082/fhir',
+  action: 'upload',
+}
+
+module.exports.globalResource = {
+  // Should be resource name
+  Patient: {
+    meta: {
+      profile: [
+        "https://hapi.fhir.tw/fhir/StructureDefinition/Patient-MITW2022-T1SC2"
+      ]
+    }
+  }
+}
+
+module.exports.fields = [
+  {
+    source: 'id',
+    target: 'Patient.id',
+    beforeConvert: (data) => {
+      return `t1s2-pat-${data}`
+    }
+  },
+  {
+    source: 'text',
+    target: 'Patient.text',
+    // text已經組合成Object，直接回傳即可，無須再透過beforeConvert:()來處理
+  },
+  {
+    source: 'age',
+    target: 'Patient.age',
+  },
+  {
+    source: 'identifier',
+    target: 'Patient.identifier',
+    beforeConvert: (data) => {
+      let identifier = data;
+      identifier.type.coding = [identifier.type.coding];
+
+      return identifier;
+    }
+  },
+  {
+    source: 'active',
+    target: 'Patient.active',
+    beforeConvert: (data) => {
+      // https://stackoverflow.com/questions/263965/how-can-i-convert-a-string-to-boolean-in-javascript
+      let booleanValue = (data.toString().toLowerCase() === "true");
+
+      return booleanValue;
+    }
+  },
+  {
+    source: 'name',
+    target: 'Patient.name',
+    beforeConvert: (data) => {
+      let name = data;
+      name.given = [name.given];
+
+      return name;
+    }
+  },
+  {
+    source: 'telecom',
+    target: 'Patient.telecom',
+  },
+  {
+    source: 'gender',
+    target: 'Patient.gender',
+  },
+  {
+    source: 'birthDate',
+    target: 'Patient.birthDate',
+  },
+  {
+    source: 'address',
+    target: 'Patient.address',
+  },
+  {
+    source: 'maritalStatus',
+    target: 'Patient.maritalStatus',
+    beforeConvert: (data) => {
+      let maritalStatus = data;
+      maritalStatus.coding = [maritalStatus.coding];
+
+      return maritalStatus;
+    }
+  },
+  {
+    source: 'photo',
+    target: 'Patient.photo',
+  },
+  {
+    source: 'contact',
+    target: 'Patient.contact',
+    beforeConvert: (data) => {
+      let contact = data;
+      contact.relationship.coding = [contact.relationship.coding];
+
+      contact.relationship = [contact.relationship];
+
+      contact.name.given = [contact.name.given];
+
+      contact.telecom = [contact.telecom];
+
+      return contact;
+    }
+  },
+  {
+    source: 'communication',
+    target: 'Patient.communication',
+    beforeConvert: (data) => {
+      let communication = data;
+      communication.language.coding = [communication.language.coding];
+
+      return communication;
+    }
+  },
+  {
+    source: 'managingOrganization',
+    target: 'Patient.managingOrganization',
+  }
+]
